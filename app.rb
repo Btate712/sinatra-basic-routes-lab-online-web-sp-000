d4d3664e08136f501a527c7b3e10d18711cf332a
Robert Tate <btate712@gmail.com>
2019-04-20 03:02:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/'
+    "Hello, World"
+  end
+
+  get '/name'
+    "My name is __"
+  end
+
+  get '/hometown'
+    "My hometown is __"
+  end
+
+  get '/favorite-song'
+    "My favorite song is __"
+  end
 end
